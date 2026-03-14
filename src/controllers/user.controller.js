8c2fba917be03010cbf5778724eaabf2513ff958
TanishqParnami <tani.parnami.kijai@gmail.com>
2026-03-14 16:43:56 +0530

    debug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -118,9 +118,15 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const { email, username, password } = req.body;
 
-  if (!username || !email) {
-    throw new ApiError(400, "Please enter password or username");
-  }
+  console.log(req.body); //check what postman sends
+
+  if (!username && !email) {
+  throw new ApiError(400, "Username or email is required");
+}
+
+if (!password) {
+  throw new ApiError(400, "Password is required");
+}
 
   const user = await User.findOne({
     $or: [{ username }, { email }],

c90a8c5a77b1e3e0b5f56c611b7bf16e01bccf9d
TanishqParnami <tani.parnami.kijai@gmail.com>
2026-03-18 01:31:32 +0530

    subscription and channel pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -364,4 +364,79 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
   )
 })
 
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+  const {username} = req.params //from url, not body
+
+  if(!username?.trim()){
+    throw new ApiError(400, "Username is missing")
+  }
+
+  
+    const channel = await User.aggregate([
+      {
+        $match: {
+          username: username?.toLowerCase()
+        } //user match
+      },
+      {
+        $lookup: { //number of subscribers
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers"
+        }
+      },
+      {
+        $lookup: { //no. of channels subscribed to by the user
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "subscribedTo"
+        }
+      },
+      {
+        $addFields: {
+          subscriberCount: {
+            $size: "$subscribers"
+          },
+          channelSubscribedToCount: {
+            $size: "$subscribedTo"
+          },
+          isSubscribed: {
+            $cond: {
+              if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+              then: true,
+              else: false
+            } //for the status that whether subscribed or not
+          }
+        }
+      },
+      {
+        $project: { 
+          fullName: 1,
+          username: 1,
+          subscriberCount : 1,
+          channelSubscribedToCount: 1,
+          isSubscribed: 1,
+          avatar: 1,
+          coverImage: 1,
+          email: 1,
+          createdAt: 1
+        }
+      }
+    ]) //pipeline likhne ke baad, return me arrays aate hai
+
+    console.log(channel)
+
+    if(!channel?.length){
+      throw new ApiError(404, "channel does not exist")
+    }
+
+    return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully")
+    )
+})
+
 export { registerUser, loginUser, logoutUser, refreshAccessToken, getCurrentUser, changeCurrentPassword, updateAccountDetails, updateUserAvatar, updateUserCoverImage };
